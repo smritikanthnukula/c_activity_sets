7d765074d6d6a308f0d8228b777af0e9833da154
smriti <smritin.bsc23@rvu.edu.in>
2024-02-25 23:09:03 +0530

    test

--- set03/problem06.c
+++ set03/problem06.c
@@ -22,28 +22,32 @@ void input_string(char* a, char* b)
 }
 int sub_str_index(char* string, char* substring)
 {
-    int string_length;
-    int substring_length;
-    for (int i =0; substring[i] !='\0' && string_length[i]!='\0' ;i++)
-    {
-        substring_length++;
-        string_length++;
-        printf("%d",substring_length);
-        printf("%d",string_length);
-    }
-    int False = -1;
-    for (int i = 0; i <= string_length - substring_length; i++) {
-        int j;
-        for (j = 0; j < substring_length; j++) {
-            if (string[i + j] != substring[j]) {
-                break;
-            }
-        }
-        if (j == substring_length) {
-            return i; 
-        }
-    }
-    return False; 
+    void slice(const char* str, char* result, size_t start, size_t end)
+{
+    strncpy(result, str + start, end - start);
+}
+    // int string_length;
+    // int substring_length;
+    // for (int i =0; substring[i] !='\0' && string_length[i]!='\0' ;i++)
+    // {
+    //     substring_length++;
+    //     string_length++;
+    //     printf("%d",substring_length);
+    //     printf("%d",string_length);
+    // }
+    // int False = -1;
+    // for (int i = 0; i <= string_length - substring_length; i++) {
+    //     int j;
+    //     for (j = 0; j < substring_length; j++) {
+    //         if (string[i + j] != substring[j]) {
+    //             break;
+    //         }
+    //     }
+    //     if (j == substring_length) {
+    //         return i; 
+    //     }
+    // }
+    // return False; 
 }
 
 
