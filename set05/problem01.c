5b6b5673081c7b543ca4225218c5e9f12cf9a8df
smriti <smritin.bsc23@rvu.edu.in>
2024-02-27 22:16:33 +0530

    test

---
+++ set05/problem01.c
@@ -0,0 +1,23 @@
+//  Write a program to find the distance between two points.
+#include<stdio.h>
+struct point {
+  float x;
+  float y;
+};
+
+typedef struct point Point;
+
+Point input();
+void dist(Point a, Point b, float *res);
+void output(Point a, Point b, float res);
+int main()
+{
+
+}
+Point input()
+{
+    Point p;
+
+}
+void dist(Point a, Point b, float *res);
+void output(Point a, Point b, float res);
