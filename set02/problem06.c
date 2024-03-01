fb332d0f26f98047a9797b2fd6dced2eb317e451
smriti <smritin.bsc23@rvu.edu.in>
2024-03-01 23:40:52 +0530

    test

--- set02/problem06.c
+++ set02/problem06.c
@@ -5,32 +5,36 @@ void str_reverse(char *str, char *rev_str);
 void output(char *a, char *reverse_a);
 int main()
 {
-    char str[100], reverse_str[100];
+    char str[100], rev_str[100];
     input_string(str);
-    str_reverse(str,reverse_str);
-    output(str,reverse_str);
+    printf("String = %s\n",str);
+    str_reverse(str,rev_str);
+    output(str,rev_str);
     return 0;
 }
 void input_string(char *a)
 {
     printf("enter the word:");
-    scanf("%s",a);
+    scanf("%[^\n]s\n",a);
+
 }
 void str_reverse(char *str, char *rev_str)
 {
-    int length=0;
-    for (int i =0; str[i] != '\0';i++)
-    {
-        length++;
-    }
-    int j=length-1;
-    for (int i = 0; i <length; i++, length--) 
-    { 
-        rev_str[i] = str[length]; 
-        // str[i] = str[j]; 
-        // str[j] = rev_str[i];
-    }
+   int length =0;
+   for (int i =0;str[i] != '\0';i++ )
+   {
+    // if (str[i] =='\0')
+    length++;
+   }
+   printf("%d\n",length);
+   int t = length;
+   for (int i=0;i<length;i++)
+   {
+    rev_str[i] = str[(t-i)-1];
+    // rev_str[i]=str[i];
+    // printf("-%c  %c\n",str[i],str[(t-i)-1]);     
 
+   }
 }
 void output(char *a, char *reverse_a)
 {
