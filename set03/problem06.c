eb46eb26dae1f67addce7739a3a428fe006dafd3
smriti <smritin.bsc23@rvu.edu.in>
2024-03-04 15:08:32 +0530

    test

--- set03/problem06.c
+++ set03/problem06.c
@@ -25,27 +25,37 @@ int sub_str_index(char* string, char* substring)
     // void slice(const char* str, char* result, size_t start, size_t end)
     // strncpy(result, str + start, end - start);
     // int i=0;
-    // int str_len=0;
-    // for ( i = 0; string[i] != '\0'; i++)
-    // {
-    //     str_len++;
-    // }
-    // printf("%d\n",str_len);
-    
-    // for ( i = 0; string[i] != '\0'; i++)
-    int i;
-    while(string[i]!='\0')
+    int substr_len=0;
+    int i=0;
+    int j=0;
+    for ( j = 0; substring[j] != '\0'; j++)
     {
-       if(substring[i] == string[i])
-       {
-        return i;
-       } 
-      i++;
+        substr_len++;
     }
-    printf("%d\n",i);
+    printf("%d\n",substr_len);
+    j =0;
+    // for ( i = 0; string[i] != '\0'; i++)
+    
+    for ( i = 0; string[i] != '\0';i++)
+    {
+        if (substr_len -1 == j)
+                {
+                   printf("%d\n",i);
+                   return i-j+1;
+                }  
+       if(substring[j] == string[i])
+            {
+                j++; 
+            } 
+          
+    }
+    return -1;
+   
     
 }
 void output(char* string, char* substring, int index)
 {
+
     printf("the index of the substring %s is %d",substring,index);
+    
 }
